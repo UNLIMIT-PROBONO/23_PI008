64b6d297d6313dfa8b874cce04510861b8d5b4f4
SEONMl <sm000117@naver.com>
2023-06-22 15:56:39 +0900

    feat: [TG-10] logo 컴포넌트 제작

---
+++ frontend/src/components/molecules/TogetherLogo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import '../css/Logo.css'
+
+const TogetherLogo = () => {
+    // 임시 로고 사용중
+    return (
+        <div class='centeredLogo'>
+            <img src={logo} width='150px' height='150px'/> 
+            <span>아울러</span>
+        </div>
+    )
+}
+
+export default TogetherLogo;
