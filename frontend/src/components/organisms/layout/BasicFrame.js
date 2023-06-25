bfe53bf19fae49be738e94d0bafd1595434d0916
SEONMl <sm000117@naver.com>
2023-06-25 16:07:41 +0900

    feat: [TG-12] basic frame layout 구현

---
+++ frontend/src/components/organisms/layout/BasicFrame.js
@@ -0,0 +1,28 @@
+import { Layout } from 'antd';
+import React, { useState } from 'react';
+import { TgtSidebar } from '../../molecules/layout/TgtSidebar';
+import { TgtHeader } from '../../molecules/layout/TgtHeader';
+
+export function BasicFrame(props){
+    const [collapsed, setCollapsed] = useState(false);
+
+    return (
+        <div>
+            <Layout>
+                <TgtSidebar 
+                    collapsed={collapsed}
+                />
+                <Layout>
+                    <TgtHeader 
+                        collapsed={collapsed}  
+                        setCollapsed={setCollapsed}  
+                    />
+                    <Layout>
+                        {props.content}
+                    </Layout>
+                    {/* Footer */}
+                </Layout>
+            </Layout>
+        </div>
+    )
+}
