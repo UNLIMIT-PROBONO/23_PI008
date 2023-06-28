3ba5c2d97493b274b58d3c0393efd3815263a422
SEONMl <sm000117@naver.com>
2023-06-28 19:07:46 +0900

    fix: api uri 수정

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,13 +1,13 @@
 const SERVER_HOST = "localhost";
 const SERVER_PORT = "8080";
-const BASE_URL = `http://${SERVER_HOST}:${SERVER_PORT}/api/`;
-
 
 // rest api URI 관리
 export const URI = {
-    LOGIN: `/login`,
-    SIGNUP: `/signup`,
-    CHECK_ID: `/signup/id`,
+    BASE_URL: `http://${SERVER_HOST}:${SERVER_PORT}/api/`,
+
+    LOGIN: `manager/login`,
+    SIGNUP: `manager/signup`,
+    CHECK_ID: `manager/signup/id`,
 };
 
 // 페이지 라우팅 URI 관리
