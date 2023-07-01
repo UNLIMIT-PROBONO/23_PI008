924725e000882a40a82d4424a27d1fa2b1f86a06
SEONMl <sm000117@naver.com>
2023-07-01 20:41:53 +0900

    feat: [TG-13] 관리 대상자 상세 정보 페이지 구현

--- frontend/src/pages/templates/TargetDetailTemplate.js
+++ frontend/src/pages/templates/TargetDetailTemplate.js
@@ -4,40 +4,9 @@ import { ManagementHistory } from '../../components/organisms/management-target/
 import { UsageComparison } from '../../components/organisms/management-target/UsageComparison';
 
 export function TargetDetailTemplate(props) {
-    const managementHistory = props.managementHistory;
-    // const targetInfomation = props.targetInfomation;
-    // const targetUsage = props.targetUsage;
-
-    const targetInfomation= {
-        "name":"이름 ",
-        "birth" : new Date(1928,3,5),
-        "address":"경기도",
-        "phone_number":"010111112222",
-    }
-    const targetUsage = {
-        "call":[
-            {
-                "id": "call",
-                "value": 1233,
-                "todayUsage" : 344,
-                "weekAvg" : 234,
-            },
-        ],
-        "water":[
-            {
-                "id": "water",
-                "todayUsage" : 233,
-                "weekAvg" : 234,
-            },
-        ],
-        "elec":[
-            {
-                "id": "elec",
-                "todayUsage" : 321,
-                "weekAvg" : 234,
-            }
-        ]
-    }
+    const managementHistory = props.data.managementHistory;
+    const targetInfomation = props.data.targetInfomation;
+    const targetUsage = props.data.targetUsage;
 
     return(
         <>
