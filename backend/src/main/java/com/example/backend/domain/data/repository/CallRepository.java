ef8314a13e9eb66805bd9363ea09cf39dbcf76c5
Dayeon-Hong <dayon0325@gmail.com>
2023-07-08 00:33:20 +0900

    feat: (TG-9) 유저별 평균 구하기

--- backend/src/main/java/com/example/backend/domain/data/repository/CallRepository.java
+++ backend/src/main/java/com/example/backend/domain/data/repository/CallRepository.java
@@ -2,6 +2,15 @@ package com.example.backend.domain.data.repository;
 
 import com.example.backend.domain.data.entity.Call;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface CallRepository extends JpaRepository<Call, Long> {
+    @Query("SELECT AVG(c.usage) FROM Call c WHERE c.userId = :userId AND c.createdAt BETWEEN :startDate AND :endDate")
+    double calculateAverageUsageByUserIdAndDateRange(@Param("userId") Long userId,
+                                                     @Param("startDate") String startDate,
+                                                     @Param("endDate") String endDate);
+
 }
