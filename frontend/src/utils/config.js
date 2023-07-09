67c32ab29e0fe169926b5e534d1d2c10f1ff8848
Park Seonmi <sm000117@naver.com>
2023-07-09 20:01:59 +0900

    fix: URI 수정 ^^..

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -2,18 +2,22 @@ const SERVER_HOST = "localhost";
 const SERVER_PORT = "8080";
 
 // rest api URI 관리
+const MANAGER= "manager";
+const SCHEDULE = "schedule";
+const MANAGEMENT_TARGET = "user";
+
 export const URI = {
     BASE_URL: `http://${SERVER_HOST}:${SERVER_PORT}/api/`,
 
-    MANAGER: "manager",
-    LOGIN: MANAGER + "/login",
-    SIGNUP: MANAGER + "/signup",
-    CHECK_ID: MANAGER + "/signup/",
+    MANAGER: `${MANAGER}`,
+    LOGIN: `${MANAGER}/login`,
+    SIGNUP: `${MANAGER}/signup`,
+    CHECK_ID: `${MANAGER}/signup/`,
 
-    MANAGEMENT_TARGET: "user",
+    MANAGEMENT_TARGET: `${MANAGEMENT_TARGET}`,
 
-    SCHEDULE: "schedule",
-    SCHEDULE_HISTORY: SCHEDULE + "/history",
+    SCHEDULE: `${SCHEDULE}`,
+    SCHEDULE_HISTORY: `${SCHEDULE}/history`,
 };
 
 // 페이지 라우팅 URI 관리
