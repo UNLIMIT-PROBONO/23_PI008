93d8f2302f2d3ea99e22bc2d15930d1eb8872681
letskuku <yujung9288@naver.com>
2023-07-14 00:19:12 +0900

    (TG-15) 매니저 정보 수정 api 구현

--- backend/src/main/java/com/example/backend/domain/managers/controller/ManagersController.java
+++ backend/src/main/java/com/example/backend/domain/managers/controller/ManagersController.java
@@ -1,8 +1,9 @@
 package com.example.backend.domain.managers.controller;
 
 import com.example.backend.domain.managers.dto.request.LoginRequestDto;
+import com.example.backend.domain.managers.dto.request.ManagerRequestDto;
 import com.example.backend.domain.managers.dto.request.SignupRequestDto;
-import com.example.backend.domain.managers.service.ManagersApiService;
+import com.example.backend.domain.managers.service.ManagersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,32 +11,37 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/manager")
 @RequiredArgsConstructor
-public class ManagersApiController {
+public class ManagersController {
 
-    private final ManagersApiService managersApiService;
+    private final ManagersService managersService;
 
+    //회원가입
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody SignupRequestDto signupRequestDto) {
-        return managersApiService.signUp(signupRequestDto);
+        return managersService.signUp(signupRequestDto);
     }
 
+    //아이디 중복 확인
     @GetMapping("/signup/{id}")
     public ResponseEntity<?> duplicateId(@PathVariable("id") String id) {
-        return managersApiService.duplicateId(id);
+        return managersService.duplicateId(id);
     }
 
+    //로그인
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDto) {
-        return managersApiService.login(loginRequestDto);
+        return managersService.login(loginRequestDto);
     }
 
+    //매니저 정보 조회
     @GetMapping("/")
     public ResponseEntity<?> getManager(@RequestHeader("Authorization") String token) {
-        return managersApiService.getManager(token);
+        return managersService.getManager(token);
     }
 
-    @GetMapping("/test")
-    public String test() {
-        return "test";
+    //매니저 정보 수정
+    @PutMapping("/")
+    public ResponseEntity<?> updateManager(@RequestHeader("Authorization") String token, @RequestBody ManagerRequestDto managerRequestDto) {
+        return managersService.updateManager(token, managerRequestDto);
     }
 }
