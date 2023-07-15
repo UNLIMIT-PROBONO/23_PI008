029eadd5a3818a9ea78c75ab9d7699a8bf5ce005
Park Seonmi <sm000117@naver.com>
2023-07-15 22:11:04 +0900

    feat: [TG-19] 날짜를 누르면 일정 추가 모달이 나타나게 설정

---
+++ frontend/src/components/molecules/calendar/TgtDatePicker.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { DatePicker, Space } from "antd";
+
+const { RangePicker } = DatePicker;
+
+export function TgtDatePicker(props) {
+  return (
+    <>
+      <RangePicker />
+    </>
+  );
+}
