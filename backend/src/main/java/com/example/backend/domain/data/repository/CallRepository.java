6cbc576b6e782843dff89a57dcd4c348d2669ef3
Dayeon-Hong <dayon0325@gmail.com>
2023-07-19 01:52:34 +0900

    (TG-9) 데이터 패키지 user 연결 repository 수정

--- backend/src/main/java/com/example/backend/domain/data/repository/CallRepository.java
+++ backend/src/main/java/com/example/backend/domain/data/repository/CallRepository.java
@@ -1,6 +1,7 @@
 package com.example.backend.domain.data.repository;
 
 import com.example.backend.domain.data.entity.Call;
+import com.example.backend.domain.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,9 +9,14 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDate;
 
 public interface CallRepository extends JpaRepository<Call, Long> {
-    @Query("SELECT AVG(c.usage) FROM Call c WHERE c.userId = :userId AND c.createdAt BETWEEN :startDate AND :endDate")
+    @Query("SELECT AVG(c.usage) FROM Call c WHERE c.users.id = :userId AND c.createdAt BETWEEN :startDate AND :endDate")
     double calculateAverageUsageByUserIdAndDateRange(@Param("userId") Long userId,
                                                      @Param("startDate") String startDate,
                                                      @Param("endDate") String endDate);
+
+    Call findFirstByUsersOrderByCreatedAtDesc(Users users);
+
+    List<Call> findAllByUsersAndCreatedAtBetween(Users users, String startDate, String endDate);
+
 
 }
--- backend/src/main/java/com/example/backend/domain/data/repository/ElectricityRepository.java
+++ backend/src/main/java/com/example/backend/domain/data/repository/ElectricityRepository.java
@@ -1,6 +1,7 @@
 package com.example.backend.domain.data.repository;
 
 import com.example.backend.domain.data.entity.Electricity;
+import com.example.backend.domain.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,11 +10,13 @@ import java.time.LocalDate;
 
 public interface ElectricityRepository extends JpaRepository<Electricity, Long> {
 
-    @Query("SELECT AVG(e.usage) FROM Electricity e WHERE e.userId = :userId AND e.createdAt BETWEEN :startDate AND :endDate")
+    @Query("SELECT AVG(e.usage) FROM Electricity e WHERE e.users.id = :userId AND e.createdAt BETWEEN :startDate AND :endDate")
     double calculateAverageUsageByUserIdAndDateRange(@Param("userId") Long userId,
                                                      @Param("startDate") String startDate,
                                                      @Param("endDate") String endDate);
 
+    Electricity findFirstByUsersOrderByCreatedAtDesc(Users users);
+    List<Electricity> findAllByUsersAndCreatedAtBetween(Users users, String startDate, String endDate);
 
 
 }
