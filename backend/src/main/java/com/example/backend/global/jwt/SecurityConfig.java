3d209363754404d4f59ee0db44de8c105736b6f5
letskuku <yujung9288@naver.com>
2023-08-07 01:17:58 +0900

    feat: (TG-28) 로그아웃 기능 구현

--- backend/src/main/java/com/example/backend/global/jwt/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/global/jwt/SecurityConfig.java
@@ -17,6 +17,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.CorsFilter;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -48,6 +50,16 @@ public class SecurityConfig {
                                 .anyRequest().authenticated() //회원가입, 로그인 이외에는 권한 필요
                 )
                 .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
+
+        //로그아웃 설정
+        http.logout()
+                .logoutUrl("/api/manager/logout") //로그아웃 요청 경로
+//                .logoutSuccessUrl("/api/manager/login") //로그아웃 처리 후 이동할 경로
+                .logoutSuccessHandler(((request, response, authentication) -> {
+                    response.setStatus(HttpServletResponse.SC_OK); //로그아웃 성공 시 200 ok 반환
+                }))
+                .invalidateHttpSession(true) //세션 정보 제거
+                .deleteCookies("accessToken"); //토큰 담긴 쿠키 제거
 
         return http.build();
     }
