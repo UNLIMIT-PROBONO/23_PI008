b1c72a79e3af7d812c01264e60f48d5902d83902
Park Seonmi <sm000117@naver.com>
2023-08-15 01:26:47 +0900

    fix: RequestDto에 Setter 추가

--- backend/src/main/java/com/example/backend/domain/managers/dto/request/LoginRequestDto.java
+++ backend/src/main/java/com/example/backend/domain/managers/dto/request/LoginRequestDto.java
@@ -1,8 +1,10 @@
 package com.example.backend.domain.managers.dto.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class LoginRequestDto {
     private String loginId;
 
