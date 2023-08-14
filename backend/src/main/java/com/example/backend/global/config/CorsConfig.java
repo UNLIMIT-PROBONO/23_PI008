8793e2785a911fc01323d8942e3c8f8012ef2afc
이유정 <90572599+letskuku@users.noreply.github.com>
2023-08-14 23:48:39 +0900

    fix: cors 오류 해결 (#36)

--- backend/src/main/java/com/example/backend/global/config/CorsConfig.java
+++ backend/src/main/java/com/example/backend/global/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true); //내 서버가 응답할 때 JSON을 자바스크립트에서 처리할 수 있게 설정
-        config.addAllowedOrigin("*"); //모든 IP에 응답 허용
+        config.addAllowedOriginPattern("*"); //모든 IP에 응답 허용
         config.addAllowedHeader("*"); //모든 header에 응답 허용
         config.addAllowedMethod("*"); //모든 post, get, put, delete, patch 요청 허용
         source.registerCorsConfiguration("/**", config);
