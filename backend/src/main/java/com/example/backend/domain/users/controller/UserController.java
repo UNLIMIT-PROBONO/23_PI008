70c93b10a4ceb236b461e055c88457cd41c18c84
letskuku <yujung9288@naver.com>
2023-08-20 02:29:55 +0900

    feat: (TG-39) 관리대상자 정보 등록 구현

--- backend/src/main/java/com/example/backend/domain/users/controller/UserController.java
+++ backend/src/main/java/com/example/backend/domain/users/controller/UserController.java
@@ -1,10 +1,11 @@
 package com.example.backend.domain.users.controller;
 
 import com.example.backend.domain.users.dto.request.UserRequestDto;
-import com.example.backend.domain.users.mapper.UserMapper;
 import com.example.backend.domain.users.service.UserService;
+import com.example.backend.global.jwt.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +18,9 @@ public class UserController {
 
     private final UserService userService;
 
-    private final UserMapper userMapper;
-
     @PostMapping
-    public ResponseEntity<Void> addUser(@RequestBody UserRequestDto userRequestDto) {
-        userService.addUser(userRequestDto);
+    public ResponseEntity<Void> addUser(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody UserRequestDto userRequestDto) {
+        userService.addUser(customUserDetails.getUsername(), userRequestDto);
         return ResponseEntity.ok().build();
     }
 
