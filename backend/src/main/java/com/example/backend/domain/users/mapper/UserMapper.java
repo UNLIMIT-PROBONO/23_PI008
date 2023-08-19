ec629d7d458731ac52b81fe870b436c3dfa54c64
letskuku <yujung9288@naver.com>
2023-08-20 02:29:19 +0900

    feat: (TG-39) Manager 엔티티 연결, Gender 타입 수정에 따른 mapper, dto 수정

--- backend/src/main/java/com/example/backend/domain/users/mapper/UserMapper.java
+++ backend/src/main/java/com/example/backend/domain/users/mapper/UserMapper.java
@@ -1,20 +1,21 @@
 package com.example.backend.domain.users.mapper;
 
+import com.example.backend.domain.managers.entity.Managers;
 import com.example.backend.domain.users.dto.request.UserRequestDto;
+import com.example.backend.domain.users.entity.Gender;
 import com.example.backend.domain.users.entity.Users;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class UserMapper {
 
-    public Users toEntity (UserRequestDto userRequestDto) {
+    public Users toEntity (UserRequestDto userRequestDto, Managers manager, Gender gender) {
         return Users.builder()
+                .managers(manager)
                 .name(userRequestDto.getName())
                 .phoneNum(userRequestDto.getPhoneNum())
                 .address(userRequestDto.getAddress())
-                .gender(userRequestDto.getGender())
+                .gender(gender)
                 .birth(userRequestDto.getBirth())
                 .health(userRequestDto.getHealth())
                 .check(userRequestDto.getCheck())
