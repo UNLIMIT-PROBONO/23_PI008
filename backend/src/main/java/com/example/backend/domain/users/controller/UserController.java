24e468fd9a13e92933769cee01199de8019143db
letskuku <yujung9288@naver.com>
2023-08-20 02:57:23 +0900

    feat: (TG-39) 단일 관리대상자 정보 조회 구현

--- backend/src/main/java/com/example/backend/domain/users/controller/UserController.java
+++ backend/src/main/java/com/example/backend/domain/users/controller/UserController.java
@@ -1,15 +1,13 @@
 package com.example.backend.domain.users.controller;
 
 import com.example.backend.domain.users.dto.request.UserRequestDto;
+import com.example.backend.domain.users.dto.response.UserResponseDto;
 import com.example.backend.domain.users.service.UserService;
 import com.example.backend.global.jwt.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,4 +22,8 @@ public class UserController {
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/{user_id}")
+    public ResponseEntity<UserResponseDto> getUser(@PathVariable("user_id") Long userId) {
+        return ResponseEntity.ok(userService.getUser(userId));
+    }
 }
--- backend/src/main/java/com/example/backend/domain/users/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/domain/users/repository/UserRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
 
-    Optional<Users> findById(Long Id);
-
+    Optional<Users> findByIdAndIsActivated(Long Id, boolean status);
 }
