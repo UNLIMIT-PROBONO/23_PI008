b35a2042ef726325c5be596b7a69299954924a9e
letskuku <yujung9288@naver.com>
2023-08-20 02:56:38 +0900

    feat: (TG-39) UserResponseDto 생성

--- backend/src/main/java/com/example/backend/domain/users/mapper/UserMapper.java
+++ backend/src/main/java/com/example/backend/domain/users/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.example.backend.domain.users.mapper;
 
 import com.example.backend.domain.managers.entity.Managers;
 import com.example.backend.domain.users.dto.request.UserRequestDto;
+import com.example.backend.domain.users.dto.response.UserResponseDto;
 import com.example.backend.domain.users.entity.Gender;
 import com.example.backend.domain.users.entity.Users;
 import org.springframework.stereotype.Component;
@@ -22,4 +23,20 @@ public class UserMapper {
                 .build();
     }
 
+    public UserResponseDto fromEntity(Users user) {
+        return UserResponseDto.builder()
+                .userId(user.getId())
+                .adminId(user.getManagers().getId())
+                .name(user.getName())
+                .phoneNumber(user.getPhoneNum())
+                .address(user.getAddress())
+                .gender(user.getGender().getName())
+                .birth(user.getBirth())
+                .health(user.getHealth())
+                .check(user.getCheck())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .isActivated(user.isActivated())
+                .build();
+    }
 }
