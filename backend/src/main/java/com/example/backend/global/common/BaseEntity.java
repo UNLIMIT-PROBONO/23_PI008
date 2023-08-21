942eadc1f2947a669537677bb6de7392588518b7
letskuku <yujung9288@naver.com>
2023-08-21 22:35:17 +0900

    feat: baseEntity 날짜 포맷 코드 수정

--- backend/src/main/java/com/example/backend/global/common/BaseEntity.java
+++ backend/src/main/java/com/example/backend/global/common/BaseEntity.java
@@ -6,10 +6,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.PreUpdate;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -21,14 +18,20 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(name = "created_at", updatable = false)
-    private String createdAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+    private String createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at")
-    private String updatedAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+    private String updatedAt;
 
     @Column(name = "is_activated", nullable = false)
     private boolean isActivated = true;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        this.updatedAt = this.createdAt;
+    }
 
     @PreUpdate
     public void onPreUpdate(){
