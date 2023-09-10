5612843417f2681779db3c56583ca601a781ce77
chelmang <105014465+chelmang@users.noreply.github.com>
2023-09-11 04:29:11 +0900

    Delete backend/src/main/java/com/example/backend/domain/schedule/exception/ScheduleNotFoundException.java

--- backend/src/main/java/com/example/backend/domain/schedule/exception/ScheduleNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.domain.schedule.exception;
-
-public class ScheduleNotFoundException extends RuntimeException {
-    public ScheduleNotFoundException() {
-        super("일정을 찾을 수 없습니다.");
-    }
-}
