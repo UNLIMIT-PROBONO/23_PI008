42c6b476328f0475ac2366cec22a696bde414e62
Seonmi <sm000117@naver.com>
2023-09-13 19:02:10 +0900

    feat: #18 전체 관리 대상자 조회 페이지 테이블 생성

---
+++ frontend/src/pages/templates/TargetListTemplate.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { TargetList } from "../../components/molecules/management-target/TargetList";
+
+export const TargetListTemplate = (props) => {
+  const targetList = props.data.targetList;
+
+  return (
+    <>
+      <TargetList source={targetList} />
+    </>
+  );
+};
