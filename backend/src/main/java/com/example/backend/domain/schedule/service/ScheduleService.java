cb3ce91c8f57e376c629cdc8c1729df7a4a9552f
서민지 <chelmang@seominjiui-MacBookPro.local>
2023-09-21 23:47:58 +0900

    (TK-45)calendar

--- backend/src/main/java/com/example/backend/domain/schedule/service/ScheduleService.java
+++ backend/src/main/java/com/example/backend/domain/schedule/service/ScheduleService.java
@@ -9,4 +9,8 @@ public interface ScheduleService {
     ScheduleResponse getSchedule(Long scheduleId);
     List<ScheduleResponse> getAllSchedules(String username);
     void deleteSchedule(Long scheduleId);
+
+    List<ScheduleResponse> getSchedulesByYearAndMonth(int year, int month);
+    List<ScheduleResponse> getThisWeekSchedules();
+
 }
